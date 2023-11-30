0f38baad83a0d1258e556d6844bfbf510d57bcb6
Kaan Uzman <55639112+akaanuzman@users.noreply.github.com>
2023-12-01 01:42:53 +0300

    shopping list vc table view created

--- ShoppingListApp/Feature/ShoppingList/ShoppingListVC.swift
+++ ShoppingListApp/Feature/ShoppingList/ShoppingListVC.swift
@@ -7,10 +7,71 @@
 
 import UIKit
 
-class ShoppingListVC: UIViewController {
+final class ShoppingListVC: UIViewController {
+    private let shoppingList = ["Egg", "Fish", "Meat", "Apple", "Orange", "Chicken", "Coco Cola"]
+
+    private lazy var searchController: UISearchController = {
+        let searchController = UISearchController()
+        searchController.searchBar.placeholder = "Search"
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.tintColor = .systemGreen
+        return searchController
+    }()
+
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(
+            UITableViewCell.self,
+            forCellReuseIdentifier: "cell"
+        )
+        tableView.delegate = self
+        tableView.dataSource = self
+        return tableView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureVC()
+        setupUI()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+        navigationItem.hidesSearchBarWhenScrolling = false
+    }
+
+    private func configureVC() {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
     }
+
+    private func setupUI() {
+        navigationItem.searchController = searchController
+        view.addSubview(tableView)
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+            
+        }
+    }
 }
+
+extension ShoppingListVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shoppingList.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = shoppingList[indexPath.row]
+        return cell!
+    }
+}
+
+ #Preview {
+    ShoppingListVC()
+ }
+  
